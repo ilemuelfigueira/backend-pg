6f5b62845ac9164ad9fa8a183038421e75e56aa5
Lemuel <lfigueiradev@gmail.com>
2023-12-08 01:42:14 -0300

    [Api] feat: serverless vercel

---
+++ serverless.js
@@ -0,0 +1,17 @@
+"use strict";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import fastify from "fastify";
+
+const app = fastify({
+  logger: true,
+});
+
+app.register(import("./app/server.js"));
+
+export default async (req, res) => {
+  await app.ready();
+  app.server.emit("request", req, res);
+};
