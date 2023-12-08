a779cd5221031b533741a0f92b4c7a99d8e3d864
Lemuel <lfigueiradev@gmail.com>
2023-12-08 02:16:27 -0300

    [test] feat: teste config vercel.json

--- api/serverless.js
+++ api/serverless.js
@@ -1,12 +1,21 @@
-import fastify from "fastify";
+"use strict";
 
-const app = fastify({
+// Read the .env file.
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// Require the framework
+import Fastify from "fastify";
+
+// Instantiate Fastify with some config
+const app = Fastify({
   logger: true,
 });
 
-app.register(import("../app/app.js"));
+// Register your application as a normal plugin.
+app.register(import("../app/plugin.js"));
 
 export default async (req, res) => {
-  await app.ready();
-  app.server.emit("request", req, res);
-};
+    await app.ready();
+    app.server.emit('request', req, res);
+}
