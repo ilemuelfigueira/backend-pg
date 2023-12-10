f986c89fb27e94b27394aa9b4f1b5fe5f151a45d
Lemuel <lfigueiradev@gmail.com>
2023-12-10 04:20:29 -0300

    [cors] feat: unlocking cors

--- app/server.js
+++ app/server.js
@@ -3,10 +3,16 @@ import fastify from "fastify";
 import dotenv from "dotenv";
 dotenv.config();
 
+import cors from "@fastify/cors";
+
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3333;
 
 const app = fastify({
   logger: true,
+});
+
+app.register(cors, {
+  origin: "*",
 });
 
 app.register(import("./routes/base.js"), {
