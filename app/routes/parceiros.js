e976bdab49a9905bd4e3ffe032ce3aff9de79cd8
Lemuel <lfigueiradev@gmail.com>
2024-04-28 04:09:35 -0300

    [parceiro] feat: find by cdpareciro

--- app/routes/parceiros.js
+++ app/routes/parceiros.js
@@ -23,4 +23,38 @@ export default async function (fastify, options) {
       });
     }
   });
+
+  fastify.get("/:cdparceiro", async (request, reply) => {
+    const { cdparceiro } = request.params;
+
+    if (!cdparceiro)
+      reply.status(400).send({
+        message: "parâmetro :cdparceiro obrigatório!",
+      });
+    try {
+      const parceiro = await knexClient
+        .raw(
+          `
+          select p.* from public.parceiro p
+          where 1=1
+            and p.cdparceiro = :cdparceiro
+        `,
+          {
+            cdparceiro,
+          }
+        )
+        .then((res) => res.rows[0]);
+
+      if (!parceiro)
+        reply.status(404).send({
+          message: "Parceiro não encontrado!",
+        });
+
+      reply.send(parceiro);
+    } catch (error) {
+      reply.status(500).send({
+        message: error.message,
+      });
+    }
+  });
 }
