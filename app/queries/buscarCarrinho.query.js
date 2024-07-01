a895e0e931e53b6d6e97c5d3a3c0c210b0719635
Lemuel <lfigueiradev@gmail.com>
2024-07-01 02:54:12 -0300

    [carrinho] feat: ajuste busca carrinho

--- app/queries/buscarCarrinho.query.js
+++ app/queries/buscarCarrinho.query.js
@@ -20,32 +20,19 @@ export const buscarCarrinhoQuery = ({
       CONCAT(pr.nmprodutotipo, ' - ', pr.nmproduto) as concat_nmproduto,
       string_agg(CONCAT(sp.nmsubprodutotipo, ' - ', sp.nmsubproduto), ', ') as concat_nmsubprodutotipo,
       string_agg(sp.nmsubproduto, ', ') as concat_nmsubproduto,
-      CONCAT('${storage_public}', COALESCE(pf2.nmpath, pf.nmpath, p.avatar, pr.banners->>0)) as nmpath,
-      (SUM(COALESCE(spp.vlsubproduto, 0)) + COALESCE(pp.vlproduto, 0)) * p.quantidade as vlpacote,
-      (SUM(COALESCE(spp.vlsubproduto, 0)) + COALESCE(pp.vlproduto, 0)) as vlpacoteunidade
+      CONCAT('${storage_public}', COALESCE(p.avatar, pr.banners->>0)) as nmpath,
+      (SUM(COALESCE(sp.vlsubproduto, 0)) + COALESCE(pr.vlproduto, 0)) * p.quantidade as vlpacote,
+      (SUM(COALESCE(sp.vlsubproduto, 0)) + COALESCE(pr.vlproduto, 0)) as vlpacoteunidade
     from pacote p
     left join pacote_item pi2 
       on pi2.cdpacote = p.cdpacote 
-    left join (
-      select * from produto_foto pf
-      limit 1
-    ) as pf
-      on pf.cdproduto = p.cdproduto 
     inner join carrinho cr
       on cr.cdcarrinho = p.cdcarrinho
       ${ignoreStatusCarrinho ? "" : `and cr.sgcarrinhosituacao = 'PEN'`}
-    left join pacote_foto pf2
-      on pf2.cdpacote = p.cdpacote
     inner join produto pr
       on pr.cdproduto = p.cdproduto
     left join sub_produto sp
       on sp.cdsubproduto = pi2.cdsubproduto
-    left join produto_preco pp 
-      on pp.cdproduto = pr.cdproduto
-      and pp.flativo = 'S'
-    left join sub_produto_preco spp
-      on spp.cdsubproduto = sp.cdsubproduto
-      and spp.flativo = 'S'
     where 1=1
       and p.cdcarrinho = '${cdcarrinho}'::uuid
     group by 
@@ -54,12 +41,10 @@ export const buscarCarrinhoQuery = ({
       p.quantidade,
       p.cdproduto, 
       pr.banners,
-      pf.nmpath, 
-      pf2.nmpath,
       pr.nmproduto, 
+      pr.vlproduto,
       pr.deproduto,
-      pr.nmprodutotipo, 
-      pp.vlproduto
+      pr.nmprodutotipo
     order by
       pr.nmprodutotipo
     ;
